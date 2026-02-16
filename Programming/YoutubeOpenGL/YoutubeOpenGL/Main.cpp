86bbb33b0ffc0984145eb3a2375a6bf256434836
juandivelzz <est.juan.dvelasquezp@unimilitar.edu.co>
2026-02-16 08:28:20 -0500

    Update Main.cpp

--- Programming/YoutubeOpenGL/YoutubeOpenGL/Main.cpp
+++ Programming/YoutubeOpenGL/YoutubeOpenGL/Main.cpp
@@ -1,13 +1,62 @@
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main()
-
 {
+   
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
 
-return 0;
+ 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+   
+    GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Hacer el contexto actual
+    glfwMakeContextCurrent(window);
 
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+  
+    glViewport(0, 0, 800, 800);
+
+    // Bucle principal
+    while (!glfwWindowShouldClose(window))
+    {
+      
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Intercambiar buffers
+        glfwSwapBuffers(window);
+
+        // Procesar eventos
+        glfwPollEvents();
+    }
+
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
+
 
